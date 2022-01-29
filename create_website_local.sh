84d02e1e14d00a2056380067196d816f70b1eb6c
Jean Feydy <jean.feydy@gmail.com>
2022-01-29 17:20:40 +0100

    Minor update to the javascript charts.

--- create_website_local.sh
+++ create_website_local.sh
@@ -5,14 +5,14 @@
 # Product on the sphere:
 python3 run.py --local --dataset product-sphere-D3-E1-M1000-N1000-inverse-distance
 python3 run.py --local --dataset product-sphere-D3-E1-M2000-N2000-inverse-distance
-python3 run.py --local --dataset product-sphere-D3-E1-M5000-N5000-inverse-distance
-python3 run.py --local --dataset product-sphere-D3-E1-M10000-N10000-inverse-distance
+#python3 run.py --local --dataset product-sphere-D3-E1-M5000-N5000-inverse-distance
+#python3 run.py --local --dataset product-sphere-D3-E1-M10000-N10000-inverse-distance
 
 # Solver on the sphere:
 python3 run.py --local --dataset solver-sphere-D3-E1-M1000-N1000-inverse-distance
 python3 run.py --local --dataset solver-sphere-D3-E1-M2000-N2000-inverse-distance
-python3 run.py --local --dataset solver-sphere-D3-E1-M5000-N5000-inverse-distance
-python3 run.py --local --dataset solver-sphere-D3-E1-M10000-N10000-inverse-distance
+#python3 run.py --local --dataset solver-sphere-D3-E1-M5000-N5000-inverse-distance
+#python3 run.py --local --dataset solver-sphere-D3-E1-M10000-N10000-inverse-distance
 
 # Create the website and compress it in a zip file:
 python3 create_website.py --latex --scatter --outputdir website
